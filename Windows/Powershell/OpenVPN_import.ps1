40c01ea21a4b7bd3ee958f061d77098da4dc691f
Waves8 <37841973+Waves8@users.noreply.github.com>
2018-05-10 10:40:25 -0700

    Update OpenVPN_import.ps1
    Added in notes and how to successfully perform the foreach loop

--- Windows/Powershell/OpenVPN_import.ps1
+++ Windows/Powershell/OpenVPN_import.ps1
@@ -1,39 +1,55 @@
 # Path to OpenVPN installation directory, also where profiles will be dropped
-$ovpnpath = "C:\Program Files (x86)\OpenVPN Technologies\OpenVPN Client"
+$ovpnpath = "\path\to\ovpn"
 
 # If statement to check if the OpenVPN Client folder exists
 if (test-path -path $ovpnpath)
   {
 
+<#
+# Looks like I was able to do it via invoke-expression, and a friend was able to get it to read the variable via &
+# So now I have a way I can "get it to work", but I have to do a lame set-location hop
+# and I also know that I might be able to get it to work via &
+$profiles = get-childitem -path "\path\to\ovpnfiles" *vpn.ovpn | select Name
+# If the folder exists, then for each .ovpn file...
+     foreach($profile in $profiles.name)
+       {
+# For each .ovpn file that exists do...
+# Import the profile
+         $profilepath = "\path\to\ovpnfiles\$profile"
+         write-host "Attempting to import profile $profilepath"
+         set-location -path "\path\to\capicli.exe"
+         invoke-expression './capicli.exe -f $profilepath importprofilefromfile'
+#>
+
 # Individual If statements since I haven't cracked how to pass a variable to Call Operator so that capicli.exe does not reference variable as literal string
 # Each If statement will attempt to import a profile after successfully confirming it exists
-    if (test-path -path "C:\Program Files (x86)\OpenVPN Technologies\OpenVPN Client\avpn.ovpn")
+    if (test-path -path "\path\to\avpn.ovpn")
       {
-        & 'C:\Program Files (x86)\OpenVPN Technologies\OpenVPN Client\core\capicli.exe' '-f' 'C:\Program Files (x86)\OpenVPN Technologies\OpenVPN Client\avpn.ovpn' 'importprofilefromfile'
+        & '\path\to\capicli.exe' '-f' '\path\to\avpn.ovpn' 'importprofilefromfile'
       }
       else
         {
           write-host "$profile does not exist."
         }
-    if (test-path -path "C:\Program Files (x86)\OpenVPN Technologies\OpenVPN Client\bvpn.ovpn")
+    if (test-path -path "\path\to\bvpn.ovpn")
       {
-        & 'C:\Program Files (x86)\OpenVPN Technologies\OpenVPN Client\core\capicli.exe' '-f' 'C:\Program Files (x86)\OpenVPN Technologies\OpenVPN Client\bvpn.ovpn' 'importprofilefromfile'
+        & '\path\to\capicli.exe' '-f' '\path\to\bvpn.ovpn' 'importprofilefromfile'
       }
       else
         {
           write-host "$profile does not exist."
         }
-    if (test-path -path "C:\Program Files (x86)\OpenVPN Technologies\OpenVPN Client\3vpn.ovpn")
+    if (test-path -path "\path\to\cvpn.ovpn")
       {
-        & 'C:\Program Files (x86)\OpenVPN Technologies\OpenVPN Client\core\capicli.exe' '-f' 'C:\Program Files (x86)\OpenVPN Technologies\OpenVPN Client\3vpn.ovpn' 'importprofilefromfile'
+        & '\path\to\capicli.exe' '-f' '\path\to\cvpn.ovpn' 'importprofilefromfile'
       }
       else
         {
           write-host "$profile does not exist."
         }
-    if (test-path -path "C:\Program Files (x86)\OpenVPN Technologies\OpenVPN Client\4vpn.ovpn")
+    if (test-path -path "\path\to\dvpn.ovpn")
       {
-        & 'C:\Program Files (x86)\OpenVPN Technologies\OpenVPN Client\core\capicli.exe' '-f' 'C:\Program Files (x86)\OpenVPN Technologies\OpenVPN Client\4vpn.ovpn' 'importprofilefromfile'
+        & '\path\to\capicli.exe' '-f' '\path\to\dvpn.ovpn' 'importprofilefromfile'
       }
       else
         {
