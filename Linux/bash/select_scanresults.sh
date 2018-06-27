517bd94611598031ae777cecce5236602a759229
Waves8 <37841973+Waves8@users.noreply.github.com>
2018-06-27 12:20:16 -0700

    Create select_scanresults.sh

---
+++ Linux/bash/select_scanresults.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+read -sp 'Please enter token: ' token
+
+curl -s -k -X GET -H "X-SecurityCenter: $token" -H 'Content-Type: application/json' -b sc_cookie.txt https://SECURITYCENTER/rest/scanResult?fields=name,description,status,finishTime | python -m json.tool
+
+dlanswer=
+while [[ "$dlanswer" -ne "1" ]]; do
+  echo "Do you wish to download the results?"
+  read -p '([Y]es/[N]o) ' download
+  case $download in
+  [yY])
+    dlanswer=1
+    ;;
+  [nN])
+    exit
+    ;;
+  *)
+    echo "Invalid input, use Y or N."
+    ;;
+  esac
+done
+
+sure=
+while [[ "$sure" -ne "1" ]]; do
+    read -p 'Enter desired ScanID: ' scanid
+    echo "$scanid"
+    echo "Correct ScanID?"
+    read -p '([Y]es/[N]o/[Q]uit) ' sure
+    case $sure in
+    [yY])
+      sure=1
+      curl -s -k -X POST -H "X-SecurityCenter: $token" -H 'Content-Type: application/json' -b sc_cookie.txt https://SECURITYCENTER/rest/scanResult/$scanid/download > scan_"$scanid".zip
+      ;;
+    [nN])
+      continue
+      ;;
+    [qQ])
+      exit
+      ;;
+    *)
+      echo "Invalid input, use Y or N or Q."
+      ;;
+    esac
+done
